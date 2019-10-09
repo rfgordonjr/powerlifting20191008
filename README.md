2581e3a144eba2cf1e81e44fb50338aa60c09ddd
Rob Gordon <rfgordonjr@gmail.com>
2019-10-08 22:09:49 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+## Tidy Tuesday: Powerlifting Oct 8, 2019
+
+This repo contains the following:
+
+- `readData.R`: ingests and explores the raw data.
+
---
+++ readData.R
@@ -0,0 +1,3 @@
+## Pull Data ####
+ipf_lifts <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-08/ipf_lifts.csv")
+str(ipf_lifts)
