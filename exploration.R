41c9b21f5e484a674b9f1b08147ec235d93dd101
Rob Gordon <rfgordonjr@gmail.com>
2019-10-08 22:58:14 -0400

    Added bar plots for number of Events per gender.

--- exploration.R
+++ exploration.R
@@ -5,6 +5,9 @@ library(here)
 ipf_data <- readRDS(file = here::here('data', 'ipf_lifts.rds'))
 str(ipf_data)
 
+## Find date range of events
+range(ipf_data$date)
+
 ## Can events last multiple days? Looks like they're all 1-day events. 
 ipf_data %>% 
   select(name, meet_name, date) %>% 
@@ -33,5 +36,40 @@ ipf_data %>%
   arrange(desc(freq)) %>% 
   slice(1:20) %>% 
   ggplot(.) +
-  geom_bar(aes(reorder(name, -freq), freq), stat = "identity")
+  geom_bar(aes(reorder(name, -freq), freq), stat = "identity") +
+  # theme(axis.text.x = element_text(angle = 90)) _
+  theme(axis.text.x=element_text(size=12, angle=90,hjust=0.95,vjust=0.2)) +
+  labs(title = "Top 20 Most Frequent Participants",
+       subtitle = paste0(format(min(ipf_data$date), "%B %d, %Y"), " - ", format(max(ipf_data$date), "%B %d, %Y")),
+       x = "Name",
+       y = "Number of Events")
+
+## Repeat but include by sex
+ipf_data %>% 
+  group_by(name, sex) %>% # nrow()
+  summarise(freq = n()) %>% 
+  arrange(sex, desc(freq)) %>%
+  ungroup() %>% 
+  group_by(sex) %>% 
+  mutate(rowNum = row_number()) %>% 
+  ungroup() %>% 
+  filter(rowNum <= 20) 
+ipf_data %>% 
+  group_by(name, sex) %>% # nrow()
+  summarise(freq = n()) %>% 
+  arrange(sex, desc(freq)) %>%
+  ungroup() %>% 
+  group_by(sex) %>% 
+  mutate(rowNum = row_number()) %>% 
+  ungroup() %>% 
+  filter(rowNum <= 20) %>% 
+  ggplot(.) +
+  geom_bar(aes(reorder(name, -freq), freq, fill = sex), stat = "identity") +
+  # theme(axis.text.x = element_text(angle = 90)) _
+  theme(axis.text.x=element_text(size=10, angle=90,hjust=0.95,vjust=0.2)) +
+  labs(title = "Top 20 Most Frequent Participants by Sex",
+       subtitle = paste0(format(min(ipf_data$date), "%B %d, %Y"), " - ", format(max(ipf_data$date), "%B %d, %Y")),
+       x = "Name",
+       y = "Number of Events")
+
   
