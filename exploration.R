23e7f5918e9a6a02c6972b8d2d28de84aabf0faf
Rob Gordon <rfgordonjr@gmail.com>
2019-10-08 23:05:26 -0400

    Top 20 events by gender facetted.

--- exploration.R
+++ exploration.R
@@ -5,10 +5,10 @@ library(here)
 ipf_data <- readRDS(file = here::here('data', 'ipf_lifts.rds'))
 str(ipf_data)
 
-## Find date range of events
+## Find date range of events ####
 range(ipf_data$date)
 
-## Can events last multiple days? Looks like they're all 1-day events. 
+## Can events last multiple days? Looks like they're all 1-day events. ####
 ipf_data %>% 
   select(name, meet_name, date) %>% 
   mutate(yearDate = lubridate::year(date)) %>% 
@@ -17,13 +17,13 @@ ipf_data %>%
   ungroup() %>% 
   arrange(desc(distinctDates))
 
-## How many unique names are there? 
+## How many unique names are there? #### 
 ipf_data %>% 
   select(name) %>% 
   distinct() %>% 
   nrow()
 
-## Which competitors have entered the most events?
+## Which competitors have entered the most events? ####
 ipf_data %>% 
   group_by(name) %>% # nrow()
   summarise(freq = n()) %>% 
@@ -44,7 +44,7 @@ ipf_data %>%
        x = "Name",
        y = "Number of Events")
 
-## Repeat but include by sex
+## Repeat but include by sex ####
 ipf_data %>% 
   group_by(name, sex) %>% # nrow()
   summarise(freq = n()) %>% 
@@ -72,4 +72,22 @@ ipf_data %>%
        x = "Name",
        y = "Number of Events")
 
-  
+## Use facet instead ####
+ipf_data %>% 
+  group_by(name, sex) %>% # nrow()
+  summarise(freq = n()) %>% 
+  arrange(sex, desc(freq)) %>%
+  ungroup() %>% 
+  group_by(sex) %>% 
+  mutate(rowNum = row_number()) %>% 
+  ungroup() %>% 
+  filter(rowNum <= 20) %>% 
+  ggplot(.) +
+  geom_bar(aes(reorder(name, -freq), freq, fill = sex), stat = "identity") +
+  facet_grid(~sex, scales = "free_x") +
+  # theme(axis.text.x = element_text(angle = 90)) _
+  theme(axis.text.x=element_text(size=10, angle=90,hjust=0.95,vjust=0.2)) +
+  labs(title = "Top 20 Most Frequent Participants by Sex",
+       subtitle = paste0(format(min(ipf_data$date), "%B %d, %Y"), " - ", format(max(ipf_data$date), "%B %d, %Y")),
+       x = "Name",
+       y = "Number of Events")  
